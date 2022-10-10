2865bea766307e205c6311c5ca844dd9f1d2c728
Bethel Arthur <kbarthur14@gmail.com>
2022-10-09 23:15:37 -0500

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,152 @@
+
+/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
+ * import express */
+const express = require('express');
+const cors = require('cors');
+// middleware
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+// `npm install mongoose`
+const mongoose = require("mongoose");
+const options = {
+    keepAlive: true,
+    connectTimeoutMS: 10000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+// mongodb+srv://<username>:<password>@cluster0.6vk0qgz.mongodb.net/?retryWrites=true&w=majority
+// You guys need to replace with your own server url and correct <username> and <password>
+const dbUrl = `mongodb+srv://BArthur1972:d5GHuV9rtNRkwsTD@atlascluster.bqqnv3b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Mongo DB connection
+mongoose.connect(dbUrl, options, (err) => {
+    if (err) console.log(err);
+});
+
+// Validate DB connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Mongo DB Connected successfully");
+});
+
+// Schema for Book
+let Schema = mongoose.Schema;
+let postSchema = new Schema(
+    {
+        postMessage: {
+            type: String,
+        }
+    },
+    { timestamps: true }
+);
+let PostModel = mongoose.model("posts", postSchema);
+
+
+app.get('/', (req, res) => {
+    res.send('You are lucky!! server is running...');
+});
+
+/** GET API: GETs Posts from DB and returns as response */
+app.get('/posts', async (req, res) => {
+    try {
+        let posts = await PostModel.find();
+        res.status(200).json({
+            status: 200,
+            data: posts.reverse(),
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+/** POST API: Gets new post info from React and adds it to DB */
+app.post('/post', async (req, res) => {
+    const inputPost = req.body;
+    console.log(inputPost);
+    const matchingPost = await PostModel.findById(inputPost._id);
+    if (matchingPost) {
+        res.status(500);
+        console.error(`Post with id:${inputPost.id} already exists`);
+    }
+    try {
+        console.log('Input Post:', inputPost);
+        let post = new PostModel(inputPost);
+        post = await post.save();
+        res.status(200).json({
+            status: 200,
+            data: post,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+
+app.put("/posts/:postId", async (req, res) => {
+    try {
+        console.log('PUT request..:' + req.params.postId);
+        let newPost = req.body;
+        console.log(JSON.stringify('Req body:', JSON.stringify(newPost)));
+        /** There is BUG, Data is not getting updated in DB for me */
+        let post = await PostModel.findByIdAndUpdate({ _id: req.params.postId }, req.body, {
+            new: true,
+        }).catch((err) => {
+            console.error('Error-----------------', err);
+        });
+        if (post) {
+            console.log(JSON.stringify(post));
+            res.status(200).json({
+                status: 200,
+                data: post,
+            });
+        }
+        res.status(400).json({
+            status: 400,
+            message: "No Post found",
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+
+/** DELETE API: Gets ID of the book to be deleted from React and deletes the book in db. 
+ * Sends 400 if there is no book with given id
+ * Sends 500 if there is an error while saving data to DB
+ * Sends 200 if deleted successfully
+ */
+app.delete("/posts/:postId", async (req, res) => {
+    try {
+        let post = await PostModel.findByIdAndRemove(req.params.postId);
+        if (post) {
+            res.status(200).json({
+                status: 200,
+                message: "Post deleted successfully",
+            });
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: "No Post found",
+            });
+        }
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+app.listen(3001);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+	const [post, setPost] = useState("");
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		getPostsFromNode();
+	}, [posts]);
+
+	const getPostsFromNode = () => {
+		fetch('http://localhost:3001/posts')
+			.then(res => res.json())
+			.then(data => {
+				setPosts(data.data);
+			});
+	};
+
+	function parseDateTime(dateTimeString) {
+		const dateTimeObject = new Date(dateTimeString);
+
+		let time = dateTimeObject.toLocaleTimeString().split(":");
+		let unit = time[2].slice(3).toLocaleUpperCase();
+		
+		let date = dateTimeObject.toDateString().split(" ").slice(1);
+
+		let formattedString = `${date[0]} ${date[1]} ${date[2]} - ${time[0]}:${time[1]} ${unit}`;
+
+		return formattedString;
+	}
+
+	const addPost = (event) => {
+		const newPost = { postMessage: post };
+
+		fetch('http://localhost:3001/post', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(newPost)
+		}).then(res => res.json()).then(body => console.log("Status of adding a post: " + body.status));
+
+		setPost("");
+	};
+
+	const deletePost = (id) => {
+		console.log('...requested to update book');
+		fetch(`http://localhost:3001/posts/${id}`, { method: 'DELETE', headers: { 'Content-Type': 'application/json' } })
+			.then(res => res.json()).then(status => console.log("Status of deleting book: " + status.status));
+	};
+
+	return (
+		<div className='react-app-component text-center'>
+			<div className="container">
+				<div className="row justify-content-md-center">
+					<div className="col-6">
+						<div className="card">
+							<div className="card-body">
+								<div className="mb-3">
+									<label className="form-label">Enter your post</label>
+									<textarea className="form-control" id="post-content" rows="3" value={post} onChange={(e) => (setPost(e.target.value))}></textarea>
+									<div className="d-grid gap-2">
+										<button type="button" className="btn btn-primary mt-2" onClick={(e) => addPost(e)}>Post</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						{
+							posts.map(((post, index) => {
+								return (
+									<div key={index} className="card text-white bg-dark my-3 text-start">
+										<div className="card-body">
+											<h6 className="card-subtitle mb-2 text-muted">{parseDateTime(post.createdAt)}</h6>
+											<p className="card-text">{post.postMessage}</p>
+											<a href="#" className="card-link" onClick={() => deletePost(post._id)}>Delete</a>
+										</div>
+									</div>
+								)
+							}))
+						}
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
